260bf056582f60325b549c307980e486ed6d61b5
Rafael Moura <rafaelmoura7033@gmail.com>
2025-06-14 15:27:35 -0300

    Reestruturando o arquivo analytics para analisar o csv de forma remota

--- app/analytics.py
+++ app/analytics.py
@@ -2,26 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import base64
+import os
+import gdown
 
-def carregar_dados(caminho_csv: str) -> pd.DataFrame:
-    # mesma lógica de leitura com fallback
-    try:
-        dados = pd.read_csv(caminho_csv, sep=',', encoding='utf-8')
-    except UnicodeDecodeError:
-        dados = pd.read_csv(caminho_csv, sep=',', encoding='latin1')
-    return dados
+def carregar_dados():
+    file_id = "14vCXaKwQSF_MMGX-WCu093Y_lvwzIfpw"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    output = "realtor-data.csv"
+
+    # Baixar somente se ainda não estiver salvo localmente
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+
+    df = pd.read_csv(output)
+
+    print("Colunas disponíveis:", df.columns.tolist())  # Para debug
+
+    return df
 
 def gerar_histograma_preco(dados: pd.DataFrame) -> str:
-    fig, ax = plt.subplots(figsize=(10,6))
+    fig, ax = plt.subplots(figsize=(10, 6))
     dados_filtrados = dados[(dados['price'] > 0) & (dados['price'] < 10_000_000)]
     dados_filtrados['price'].hist(bins=50, ax=ax)
     ax.set_title('Distribuição dos preços dos imóveis')
     ax.set_xlabel('Preço')
     ax.set_ylabel('Quantidade de imóveis')
-    
+
     buf = io.BytesIO()
     plt.savefig(buf, format="png")
     buf.seek(0)
-    img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    img_base64 = base64.b64encode(buf.read()).decode("utf-8")
     plt.close()
     return img_base64
