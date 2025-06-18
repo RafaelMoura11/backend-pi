a93d98b5f4812a9a73168ecde9df23027d84f969
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2025-06-17 23:05:42 -0300

    Refactor main.py to include authentication router and database setup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
-from app.api import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.api import router as api_router
+from app.auth import router as auth_router
+from app.models import Base
+from app.database import engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Liberar requisições do frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ou restrinja para ["http://localhost:3000"] em produção
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(api_router)
+app.include_router(auth_router)  
